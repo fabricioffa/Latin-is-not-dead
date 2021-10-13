b755d702ecc9b3d469830aba21107d36f76bae51
Fabricio de Freitas <fabricioffa08@gmail.com>
2021-10-13 10:43:53 -0300

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const menuLinks = document.querySelectorAll('.menu a');
+
+menuLinks.forEach(link => link.addEventListener('click', e => {
+    const hamburguerToogle = document.querySelector('#hamburguer-toogle');
+    
+}))
+
+console.log(menuLinks);
