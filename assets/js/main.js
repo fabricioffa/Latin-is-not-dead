c4c4eeefb3352a8fff42700519ce0656d79589fe
Fabricio de Freitas <fabricioffa08@gmail.com>
2021-10-13 17:26:01 -0300

    menu and grids: before aplying deboucing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
 const menuLinks = document.querySelectorAll('.menu a');
 const grids = document.querySelectorAll('.grid');
-const gridtal = document.querySelector('.course-grid');
 
 
 // MENU
@@ -16,31 +15,31 @@ menuLinks.forEach(link => link.addEventListener('click', e => {
 // GRIDS
 
 class GridControlers {
-
+    
     countColumns(grid) {
         const columnsNumber = window.getComputedStyle(grid)
-            .getPropertyValue('grid-template-columns')
-            .split(' ')
-            .length;
+        .getPropertyValue('grid-template-columns')
+        .split(' ')
+        .length;
         return columnsNumber;
     }
-
+    
     countRows(grid) {
         const rowsNumber = window.getComputedStyle(grid)
-            .getPropertyValue('grid-template-rows')
-            .split(' ')
-            .length;
+        .getPropertyValue('grid-template-rows')
+        .split(' ')
+        .length;
         return rowsNumber;
     }
-
+    
     countItems(grid) {
         const itemsNum = this.countColumns(grid) * this.countRows(grid);
         return itemsNum;
     }
-
+    
     hideRows(grid, itemSelector) {
         this.showRows(grid, itemSelector);
-
+        
         if (this.countRows(grid) > 1) {
             const gridItems = Array.from(grid.querySelectorAll(itemSelector));
             const colsPerRow = this.countColumns(grid);
@@ -48,34 +47,48 @@ class GridControlers {
             
             for (let col of otherRowsCols) {
                 col.style.display = 'none';
-            }
+            };
         }
+        
+        return this;
     }
     
     showRows(grid, itemSelector) {
         const gridItems = Array.from(grid.querySelectorAll(itemSelector));
         const colsPerRow = this.countColumns(grid);
         const firstRowCols = gridItems.slice(0, colsPerRow);
-
+        
         for (let col of firstRowCols) {
             col.style.display = 'block';
-        }
+        };
+        
+        return this;
     }
-
+    
+    formatGrids(grids) {
+        
+        grids.forEach(grid => {
+            
+            if (grid.classList.contains('intro-grid')) return;
+            
+            const gridItemClass = grid.querySelector('div').classList[0];
+            
+            this.hideRows(grid, `.${gridItemClass}`)
+            // this.hideRows(grid, `.${gridItemClass}`);
+        });
+        
+        return this;
+    }
+    
 }
 
+const gridControlers = new GridControlers;
+
 window.addEventListener('resize', () => {
-    
-    const gridControler = new GridControlers;
-    
-    
-    grids.forEach(grid => {
-        
-        if (grid.classList.contains('intro-grid')) return;
-        
-        const gridItemClass = grid.querySelector('div').classList[0];
-        gridControler.hideRows(grid, `.${gridItemClass}`);
-    });
-    
+    gridControlers.formatGrids(grids);
+});
+
+window.addEventListener('load', () => {
+    gridControlers.formatGrids(grids);
 });
 
