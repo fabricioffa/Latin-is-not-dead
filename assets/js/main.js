273ac99d94c4cf44b4df0e39f9c82aad8d2e7f07
Fabricio <fabricioffa08@gmail.com>
2021-11-14 09:48:49 -0300

    Added event listener to arrow-up to control its display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,12 @@
 import gridController from "./GridController.js";
 import FormValidator from "./FormValidator.js";
 
-const menuLinks = document.querySelectorAll('.menu a');
-const grids = document.querySelectorAll('.grid');
-const contactUs = document.querySelector('.contact form');
-const slideBtns = document.querySelectorAll('.slide-btn');
-const questions = document.querySelectorAll('.question');
+const menuLinks = document.querySelectorAll(".menu a");
+const grids = document.querySelectorAll(".grid");
+const contactUs = document.querySelector(".contact form");
+const slideBtns = document.querySelectorAll(".slide-btn");
+const questions = document.querySelectorAll(".question");
+const arrowUp = document.querySelector(".arrow-up");
 
 const formValidator = new FormValidator(contactUs);
 
@@ -13,65 +14,85 @@ const formValidator = new FormValidator(contactUs);
 
 // Toggles off the menu when a link is clicked
 
-menuLinks.forEach(link => link.addEventListener('click', e => {
-  let hamburgerToggle = document.querySelector('#hamburger-toggle');
-  hamburgerToggle.checked = false;
-}))
+menuLinks.forEach((link) =>
+  link.addEventListener("click", (e) => {
+    let hamburgerToggle = document.querySelector("#hamburger-toggle");
+    hamburgerToggle.checked = false;
+  })
+);
 
 //  GRIDS
 
 // Hides all rolls but the first, every time the the window is resized
-  
-  //  The setTimeout and clearTimeout serve to limit the number of time the function is called:
-  //  If the event occur again before the delay, then the last call is aborted 
-let timeout = false; 
+
+//  The setTimeout and clearTimeout serve to limit the number of time the function is called:
+//  If the event occur again before the delay, then the last call is aborted
+let timeout = false;
+let throttled = false;
 const delay = 250;
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   clearTimeout(timeout);
   timeout = setTimeout(() => gridController.formatGrids(grids), delay);
 });
 
-  // The format is also applied on load event
+window.addEventListener("scroll", () => {
+    if (!throttled) {
+      if (window.scrollY >= document.documentElement.clientHeight) {
+        arrowUp.style.display = "inline-block";
+        return;
+      }
+      
+      arrowUp.style.display = "none";
 
-window.addEventListener('load', () => {
+      throttled = true;
+
+      setTimeout(() => throttled = false, delay);
+    }      
+});
+
+window.addEventListener("load", () => {
   gridController.formatGrids(grids);
 });
 
 /* Checks for clicks on slideBtns, then calls the slideGrid function
  with the direction parameter that corresponds the button position. */
 
-  // The buttons were all put one before the other after the grid, so as to facilitate getting the grid it is associated with. 
+// The buttons were all put one before the other after the grid, so as to facilitate getting the grid it is associated with.
 
-slideBtns.forEach(btn => btn.addEventListener('click', (e) => {
-  if (btn.classList.contains('fa-caret-right')) {
-    const grid = e.target.nextElementSibling;
+slideBtns.forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    if (btn.classList.contains("fa-caret-right")) {
+      const grid = e.target.nextElementSibling;
 
-    gridController.slideGrid(grid, 'right');
+      gridController.slideGrid(grid, "right");
 
-    return;
-  }
+      return;
+    }
 
-  const grid = e.target.previousElementSibling;
-  gridController.slideGrid(grid, 'left');
-}));
+    const grid = e.target.previousElementSibling;
+    gridController.slideGrid(grid, "left");
+  })
+);
 
 //  * F.A.Q.
 
-// Shows or hides the answers every time a question is clicked 
+// Shows or hides the answers every time a question is clicked
 
-questions.forEach(question => question.addEventListener('click', e => {
-  const answer = question.querySelector('p');
+questions.forEach((question) =>
+  question.addEventListener("click", (e) => {
+    const answer = question.querySelector("p");
 
-  // As at first there is no value in the style attribute, it checks for an empty string, so the first click works
+    // As at first there is no value in the style attribute, it checks for an empty string, so the first click works
 
-  if (answer.style.display === '') {
-    return answer.style.display = 'block';
-  }
+    if (answer.style.display === "") {
+      return (answer.style.display = "block");
+    }
 
-  if (answer.style.display === 'none') {
-    return answer.style.display = 'block';
-  }
+    if (answer.style.display === "none") {
+      return (answer.style.display = "block");
+    }
 
-  answer.style.display = 'none';
-}))
+    answer.style.display = "none";
+  })
+);
